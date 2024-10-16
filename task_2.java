64ce232c6e9c4b733531c2c9e8be81d98603893a
andreevaiva <i.andreeva26@acsbg.org>
2024-10-16 13:28:17 +0300

    Create task_2.java

---
+++ task_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+   public static void main(String[] args) {
+       Scanner reader=new Scanner(System.in);
+       System.out.print("Enter code: ");
+       String code = reader.nextLine();
+       switch (code){
+           case "A1":
+               System.out.println("Product \"Soda\" - Price: $1.50");
+               break;
+           case "B2":
+               System.out.println("Product \"Chips\" - Price: $1.25");
+               break;
+           case "C3":
+               System.out.println("Product \"Candy\" - Price: $0.75");
+               break;
+           case "D4":
+               System.out.println("Product \"Water\" - Price: $1.00");
+               break;
+           default:
+               System.out.println("This code is invalid.");
+       }
+   }
+}
